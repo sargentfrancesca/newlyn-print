b3ad73d941b068f0f0481b073fff28f64c88559d
Francesca Sargent <ladyquas@francescas-mbp.home>
2015-11-04 17:05:41 +0000

    added printer commands

--- print.py
+++ print.py
@@ -11,9 +11,11 @@ def getRecent():
 
 def setupPrinter(last_id):
 	post_id = getRecent()['id']
+	name = getRecent()['name']
+	body = getRecent()['body']
 
 	if post_id > last_id:
-		printer()
+		print_(name,body)
 	else:
 		pass
 	
@@ -21,10 +23,32 @@ def setupPrinter(last_id):
 
 	return last_id
 
-def printer():
-	name = getRecent()['name']
-	body = getRecent()['body']
-	os.system('say "' + body + '"')
+def print_(name, body):	
+	p = printer.ThermalPrinter(serialport="/dev/ttyAMA0")
+	name_text = textwrap.fill(name, 32)
+	body_text = textwrap.fill(body, 32)
+	spaces = "                                "
+	cut = "- - - - - - - - - - - - - - - - "
+	spaces_text = textwrap.fill(spaces, 32)
+	cut_text = textwrap.fill(cut, 32)
+	print name + spaces +body
+	os.system('say "' + name + spaces +body + '"')
+
+	p.bold_on()
+	p.print_text(name_text)
+	p.bold_off()
+	p.linefeed()
+	p.underline_on()
+	p.print_text(spaces_text)
+	p.underline_off()
+	p.linefeed()
+	p.print_text(body_text)
+	p.linefeed()
+	p.print_text(spaces_text)
+	# p.print_text(wrapped_text)
+	# p.linefeed()
+
+
 
 while 1:	
 	last_id = setupPrinter(last_id)
